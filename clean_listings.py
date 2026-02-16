9b736950422f4de61d06c4948d37eb15fc374a33
Martu <lasupermartu15@gmail.com>
2026-02-16 18:37:37 -0300

    Change data type from object to date time

--- clean_listings.py
+++ clean_listings.py
@@ -1,25 +1,23 @@
 import pandas as pd
-import chardet
-
-with open('dataset/Airbnb Data/Listings.csv', 'rb') as f:
-    result = chardet.detect(f.read())
-    encoding_type = result['encoding']
-    print(f"Detected encoding: {encoding_type}")
     
-df=pd.read_csv('dataset/Airbnb Data/Listings.csv', encoding=encoding_type)
+df=pd.read_csv('dataset/Airbnb Data/Listings.csv', encoding="mac_roman")
 
 #count: 279712
 #drop null values
 df.dropna(subset=['listing_id'])
 
-#drop duplicate values
+# #drop duplicate values
 df.drop_duplicates()
 
-#drop unnecesary columns
+# #drop unnecesary columns
 columns =['host_response_rate','host_acceptance_rate','host_is_superhost','host_has_profile_pic', 'host_identity_verified',
-         'latitude', 'longitude', 'accommodates','review_scores_accuracy', 'review_scores_communication',
-         'review_scores_value', 'instant_bookable']
+          'latitude', 'longitude', 'accommodates','review_scores_accuracy', 'review_scores_communication',
+          'review_scores_value', 'instant_bookable']
 
-print(df['name'])
+df= df.drop(columns, axis=1)
+
+#change the data type of columns
+df['host_since']= pd.to_datetime(df['host_since'],format='%Y-%m-%d')
+print(df.dtypes)
       
 #https://www.youtube.com/playlist?list=PLxJ3eugu174JqpqulHkIf0wEmA2b5N5DF
